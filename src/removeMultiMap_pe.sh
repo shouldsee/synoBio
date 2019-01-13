b9bfc4f274c546bd1fdf0debe74f44fd6ffc2c33
shouldsee <shouldsee@github.com>
2019-01-13 21:02:40 +0000

    []removeMultiMap

---
+++ src/removeMultiMap_pe.sh
@@ -0,0 +1,13 @@
+main(){	
+	### [depend]s on deOrphan.py
+	### Inplace
+	local PROG=removeMultiMap
+	local IN=$1
+	#local INPLACE={2:-1}
+	local ALI=`basename ${IN%.*}`
+	local CMD="samtools view -hf 0x2 -S $IN | grep -v \"XS:i:\"  | deOrphan.py"
+	echo $CMD 1>&2
+	[[ $DRY -eq 1 ]] ||  eval "$CMD" 
+# 	[[ $DRY -eq 1 ]] ||  runWithTimeLog "$CMD" 
+}
+main "$@"

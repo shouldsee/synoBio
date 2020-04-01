b2c6be79db9fb0dc1ec63fcde66430897b5c3932
Birte Schwarz <60658697+schwarz-b@users.noreply.github.com>
2020-04-01 17:33:49 +0200

    Changed dependencies to private server

--- synoBio/config_Bd21-3.sh
+++ synoBio/config_Bd21-3.sh
@@ -3,12 +3,13 @@
 #### $GFF,$GTF,$IDX_HISAT,$GSIZE should have been defined before proceeds
 
 ####### Adapter FASTA
-ADADIR="/home/feng/ref/adapters"
-export FA_ADAPTER_SE="$ADADIR/TruSeq3-SE.fa"
-export FA_ADAPTER_PE="$ADADIR/TruSeq3-PE-all.fa"
+ADADIR_SE="/home/birte/IGZ_data/software/Trimmomatic-0.39/adapters"
+ADADIR_PE="/home/birte/IGZ_data/ref/adapters"
+export FA_ADAPTER_SE="$ADADIR_SE/TruSeq3-SE.fa"
+export FA_ADAPTER_PE="$ADADIR_PE/TruSeq3-PE-all.fa" #Birte: not sure what is the benefit of not using TruSeq3-PE-2.fa
 
 export GNAME="Bd21-v3-1"
-export REF=/home/feng/ref/Brachypodium_Bd21_v3-1
+export REF=/home/birte/IGZ_data/ref/Brachypodium_Bd21_v3-1
 ###### Genome annotation .gtf and .gff3 (optional)
 export GTF=$(echo $REF/annotation/*.gtf)
 export GFF=$(echo $REF/annotation/*.gene_exons.gff3)
@@ -23,8 +24,8 @@ A=$(ls -1 $REF/sequence/HISAT2Index/* | head -1)
 export IDX_HISAT2=${A%%.*}
 
 export DB_MOTIF=" \
-/home/feng/ref/motif/CIS-BP/Brachypodium_distachyon.meme \
-/home/feng/ref/motif/ARABD/ArabidopsisPBM_20140210.meme"
+/home/birte/IGZ_data/ref/motif/CIS-BP/Brachypodium_distachyon.meme \
+/home/birte/IGZ_data/ref/motif/ARABD/ArabidopsisPBM_20140210.meme"
 
 export DEFLINE=$(echo $REF/annotation/*defline.txt)
 

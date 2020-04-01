f50ba658405df07f55a903f1d01527f16c6ed787
Birte Schwarz <60658697+schwarz-b@users.noreply.github.com>
2020-04-01 19:20:32 +0200

    Changed dependencies, added Samtools hardlink

--- synoBio/init.sh
+++ synoBio/init.sh
@@ -13,19 +13,21 @@ mkdir -p $JARLIB
 
 ### Trimmomatic
 # must supply absolute paths
-TRIMDIR=/home/Program_NGS_sl-pw-srv01/Trimmomatic-0.32
-printf '#!/usr/bin/env bash \njava -jar $TRIMDIR/trimmomatic-0.32.jar "$@"'>$ENVDIR/bin/trimmomatic
+TRIMDIR=/home/birte/IGZ_data/software/Trimmomatic-0.39
+printf '#!/usr/bin/env bash \njava -jar $TRIMDIR/trimmomatic-0.39.jar "$@"'>$ENVDIR/bin/trimmomatic
 
 
 
 
 ### HISAT2
-ln -sf /home/Program_NGS_sl-pw-srv01/hisat2-2.1.0/* $ENVDIR/bin
+ln -sf /home/birte/IGZ_data/software/hisat2-2.1.0/* $ENVDIR/bin
 ### picard for dedup
-ln -sf /home/Program_NGS_sl-pw-srv01/picard-tools-1.103/*.jar $JARLIB
+#ln -sf /home/Program_NGS_sl-pw-srv01/picard-tools-1.103/*.jar $JARLIB
+ln -sf /home/birte/IGZ_data/software/picard.jar $JARLIB #Birte: different from before logic, no picard-tools folder anymore
 ### StringTie
-ln -sf /home/Program_NGS_sl-pw-srv01/stringtie-1.3.3b.Linux_x86_64/stringtie $ENVDIR/bin
-
+ln -sf /home/birte/IGZ_data/software/stringtie-2.1.1.Linux_x86_64/stringtie $ENVDIR/bin
+### Samtools
+ln -sf /home/birte/IGZ_data/software/samtools-1.10/samtools $ENVDIR/bin #Birte: not tested yet if this works
 
 ##### Assumed installed
 # fastqc

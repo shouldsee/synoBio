99dad79ae24a06d17ef493bca8631f640f1ec1bd
Birte Schwarz <60658697+schwarz-b@users.noreply.github.com>
2020-04-01 17:16:59 +0200

    Changed dependencies to private server

--- synoBio/config_Ath_TAIR10.sh
+++ synoBio/config_Ath_TAIR10.sh
@@ -3,12 +3,12 @@
 #### $GFF,$GTF,$IDX_HISAT,$GSIZE should have been defined before proceeds
 
 ####### Adapter FASTA
-ADADIR="/home/Program_NGS_sl-pw-srv01/Trimmomatic-0.32/adapters"
+ADADIR="/home/birte/IGZ_data/software/Trimmomatic-0.39/adapters"
 export FA_ADAPTER_SE="$ADADIR/TruSeq3-SE.fa"
 export FA_ADAPTER_PE="$ADADIR/TruSeq3-PE-2.fa"
 
 export GNAME="Ath-TAIR10"
-export REF=/home/feng/ref/Arabidopsis_thaliana_TAIR10
+export REF=/home/birte/IGZ_data/ref/Arabidopsis_thaliana_TAIR10
 ###### Genome annotation .gtf and .gff3 (optional)
 export GTF=$(echo $REF/annotation/*.gtf)
 export GFF=$(echo $REF/annotation/*.gff*)
@@ -23,7 +23,7 @@ A=$(ls -1 $REF/sequence/HISAT2Index/* | head -1)
 export IDX_HISAT2=${A%%.*}
 
 export DB_MOTIF=" \
-/home/feng/ref/motif/ARABD/ArabidopsisPBM_20140210.meme\
+/home/birte/ref/motif/ARABD/ArabidopsisPBM_20140210.meme\
 "
 #/home/feng/ref/motif/CIS-BP/Brachypodium_distachyon.meme \
 
